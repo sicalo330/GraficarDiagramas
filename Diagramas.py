1757be7efaace7f3eae26799bf7acf3b14ea4b32
sicalo330 <noah.esteban9@gmail.com>
2026-02-13 11:35:34 -0500

    Hacer un testeo de lal ibrería railroad

--- Diagramas.py
+++ Diagramas.py
@@ -1,5 +1,12 @@
 from railroad import Diagram, Sequence, Choice, Terminal, NonTerminal, ZeroOrMore
 
+"""
+Esto solo fue un testeo de la librería railroad no lo vean como avances del taller en sí IMPORTANTE LEER
+Este código lo que hace es hacer un diagrama del texto grammarTest.txt, denle a compilar y se creará un archivo .svg
+Posteriormente pueden hacer doble click en el explorador de archivos para visualizarlo
+
+Recuerden poner .\.venv\Scripts\Activate para activar el entorno jeje
+"""
 def T(x): return Terminal(x)
 def N(x): return NonTerminal(x)
 
